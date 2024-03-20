c9e40d1de39190986e5d476a30af814c9601d34a
FranZoia6 <francozoiamoque@hotmail.com>
2024-03-20 20:37:46 +0100

    Punto 3 y punto 4

--- TP1/P1.py
+++ TP1/P1.py
@@ -63,22 +63,24 @@ with open("terminos.txt", "w") as f:
             unique_terms += 1
             terms_length += len(term)
 
-print("Cantidad de archivos", cant_files  )
-print("Cantidad de tokens:", cant_tokens )
-print("Cantidad de terminos:", cant_terms )
-print("promedio de tokens y terminos en los documentos", cant_tokens/cant_files,cant_terms/cant_files)
-print("Largo promedio de un término", terms_length/cant_terms)
-print("Cantidad de tokens del documento más corto y del más largo", cant_max_tokens, cant_min_tokens)
-print("Cantidad de tokens del documento más corto y del más largo", cant_max_terms, cant_min_tokens)
-print("Cantidad de términos que aparecen sólo 1 vez en la colección", unique_terms)
+with open("estadisticas.txt", "w") as f:
+    f.write(f"Cantidad de archivos {cant_files} \n")
+    f.write(f"Cantidad de tokens:, {cant_tokens} \n")
+    f.write(f"Cantidad de terminos: {cant_terms} \n")
+    f.write(f"promedio de tokens y terminos en los documentos {cant_tokens/cant_files,cant_terms/cant_files}\n")
+    f.write(f"Largo promedio de un término {terms_length/cant_terms}\n")
+    f.write(f"Cantidad de tokens del documento más corto y del más largo {cant_max_tokens} {cant_min_tokens}\n")
+    f.write(f"Cantidad de tokens del documento más corto y del más largo {cant_max_terms} {cant_min_tokens}\n")
+    f.write(f"Cantidad de términos que aparecen sólo 1 vez en la colección {unique_terms}\n")
 
 higher_frequency = sorted_terms[:10]
 lower_frequency = sorted_terms[-10:] 
-print("La lista de los 10 términos más frecuentes")
-for term, info in higher_frequency:
-    print(term, info['cf'])
-print("La lista de los 10 términos menos frecuentes")
-for term, info in lower_frequency:
-    print(term, info['cf'])
+with open("frecuencias.txt", "w") as f:
+    f.write(f"La lista de los 10 términos más frecuentes\n")
+    for term, info in higher_frequency:
+        f.write(f"{term} {info['cf']} \n")
+    f.write(f"La lista de los 10 términos menos frecuentes\n")
+    for term, info in lower_frequency:
+        f.write(f"{term} {info['cf']} \n")
 
 
